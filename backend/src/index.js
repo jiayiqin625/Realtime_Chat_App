9941fcee5e708a5e9e741b5f8c1d49da8f536898
Jiayi Qin <jiayiqin0625@gmail.com>
2026-09-08 13:19:29 -0700

    Add user and message models; integrate clerk middleware in server setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,8 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 
+import { clerkMiddleware } from "@clerk/express";
+
 import { connectDB } from "./config/db.js";
 //import { Routes } from "./routes/Routes.js";
 
@@ -9,6 +11,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors({ orgin: "http://localhost:5237", credentials: true }));
+app.use(clerkMiddleware);
 
 //app.use("api/", Routes);
 
